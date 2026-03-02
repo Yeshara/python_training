6770d8db6092364e71e02ef35aa88f5bafde4ba2
Lefika <klefika@deloitte.co.za>
2026-03-02 12:09:43 +0200

    My exerciseKabelo

---
+++ practice_exercise_Kabelo.py
@@ -0,0 +1,21 @@
+#Section 3
+
+# 🟢 EASY
+temperature = 23
+
+#if temperature > 25:
+#    print("Hot")
+#else: 
+ #   print("Cold")
+
+ # 🟡 MEDIUM:
+# Add "Comfortable" for 15–24
+
+if temperature > 25:
+    print("Hot")
+elif temperature >= 15 and temperature <= 24:
+    print('Comfortable')
+else:
+    print("Cold")
+
+    
