1146bafa878fbce3b9c1d4cb45ee245dd72c9b6d
Nonyane <knonyane@deloitte.co.za>
2026-03-02 11:36:14 +0200

    Yeshara practice Module 1

---
+++ Practice_Example_Kgotlello.py
@@ -0,0 +1,13 @@
+#Exercise 1
+
+Name = input("Please enter your name: ")
+Age = input("Please enter your age: ")
+print(f"Hello, my name is {Name} and I am {Age} years old.")
+
+#Exercise 2
+price = 49.99
+quantity = 3
+
+Total_cost=price*quantity
+
+print(f"Total cost: R{Total_cost:.2f}")
